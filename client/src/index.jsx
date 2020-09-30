4d36c1087ef53a863550d233028921dd76816061
Lisa Hou <lqnhou@gmail.com>
2020-09-29 19:51:01 -0700

    created react component for selecting bundling products. added basic css to distinguish containers

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,34 +2,42 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
 import axios from 'axios';
+import BundleSelect from './components/BundleSelect.jsx';
 
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      products: null
+      products: null,
+      currentProduct: null
     };
     this.getProducts = this.getProducts.bind(this);
   }
 
-  componentDidMount() {
+  componentWillMount() {
     this.getProducts();
   }
 
   getProducts() {
     return axios.get('/middle')
-    .then(data
-      => this.setState({products: data})); //currently grabs all the products and stores them in products
+    .then(data => this.setState({products: data.data, currentProduct: data.data[0]}, () => console.log(this.state.currentProduct.recc_prod_names[0]))); //currently grabs all the products and stores them in products
   }
 
   render() {
-    return <div className="grid-container">
+    return (<div className="grid-container">
+      {this.state.currentProduct ?
+      <div>
       <div className="grid-item" id="image-display">
       </div>
       <div className="grid-item" id="product-select">
+        <BundleSelect product={this.state.currentProduct}/>
       </div>
-    </div>;
+      </div>
+      :
+      null
+      }
+    </div>);
   }
 }
 
