b1cdbe57bad6114f7f9f0db45ecb948b49a44948
Lisa Hou <lqnhou@gmail.com>
2020-10-05 12:04:04 -0700

    changed another typo

--- database-mongodb/index.js
+++ database-mongodb/index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoUri = 'mongodb://localhost/middle';
+const mongoUri = 'mongodb://localhost/FECmiddle';
 
 const db = mongoose.connect(mongoUri, {useNewUrlParser: true, useUnifiedTopology: true});
 
