2ec51576719a149827c167a64c3cba581db509c7
Lisa Hou <lqnhou@gmail.com>
2020-10-05 13:29:38 -0700

    fixed typo in server test

--- tests/server.test.js
+++ tests/server.test.js
@@ -4,7 +4,7 @@ const request = supertest(app)
 
 it('Gets the /middle endpoint', async done => {
   // Sends GET Request to /middle endpoint
-  const res = await request.get('/middle')
+  const res = await request.get('/FECmiddle')
   expect(res.status).toBe(200) //makes sure 200 status code was sent
   expect(res.body.length).toBe(100) //makes sure that 100 docs were pulled from DB
   done()
