260b01c6c2a473d0e69656ef6c3c2cbab7669bf5
Lisa Hou <lqnhou@gmail.com>
2021-02-25 16:01:57 -0800

    added server tests

--- tests/server.test.js
+++ tests/server.test.js
@@ -2,10 +2,28 @@ const app = require('../server/index.js')
 const supertest = require('supertest')
 const request = supertest(app)
 
-it('Gets the /middle endpoint', async done => {
-  // Sends GET Request to /middle endpoint
-  const res = await request.get('/middle/0')
-  expect(res.status).toBe(200) //makes sure 200 status code was sent
-  //expect(res.body.length).toBe(100) //makes sure that 100 docs were pulled from DB
-  done()
-})
+describe('Sample Test', () => {
+  it('should test that true === true', () => {
+    expect(true).toBe(true)
+  })
+});
+
+describe('Test the root path', () => {
+  test('It should respond to GET request with status code 200', () => { return
+    request
+      .get('/')
+      .then( (res) => {
+        expect(res.statusCode).toBe(200);
+      });
+  });
+});
+
+describe('Test the /middle endpoint', () => {
+  test('It should respond to a GET request with status code 200', () => {
+    return request
+      .get('/middle/0')
+      .then((res) => {
+        expect(res.statusCode).toBe(200);
+      });
+  });
+});
